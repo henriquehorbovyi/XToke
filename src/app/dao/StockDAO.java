3be44df4a36992e8a439f9838e0e2e622bb84fb4
henrywm <carloshenry.wm@gmail.com>
2017-04-19 19:59:19 -0300

    Stock stuff

---
+++ src/app/dao/StockDAO.java
@@ -0,0 +1,7 @@
+package app.dao;
+
+/**
+ * Created by henry on 19/04/17.
+ */
+public class StockDAO {
+}
---
+++ src/app/model/Stock.java
@@ -0,0 +1,7 @@
+package app.model;
+
+/**
+ * Created by henry on 19/04/17.
+ */
+public class Stock {
+}
